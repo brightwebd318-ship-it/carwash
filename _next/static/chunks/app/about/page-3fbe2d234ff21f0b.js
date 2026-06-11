dbba71d7e846e140d5426505c0c3ad2052a59c75
shanshajis <shanshaji4477@gmail.com>
2026-06-11 19:40:51 +0530

    Deploy static Next.js site to gh-pages with prepended basePaths

---
+++ _next/static/chunks/app/about/page-3fbe2d234ff21f0b.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{1242:function(e,t,r){Promise.resolve().then(r.bind(r,8792))},8792:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return u}});var n=r(2265),i={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let hasA11yProp=e=>{for(let t in e)if(t.startsWith("aria-")||"role"===t||"title"===t)return!0;return!1};var o=r(2963);/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let s=(0,n.createContext)({}),useLucideContext=()=>(0,n.useContext)(s),u=(0,n.forwardRef)(({color:e,size:t,strokeWidth:r,absoluteStrokeWidth:s,className:u="",children:a,iconNode:l,...c},h)=>{let{size:d=24,strokeWidth:f=2,absoluteStrokeWidth:m=!1,color:k="currentColor",className:w=""}=useLucideContext()??{},C=s??m?24*Number(r??f)/Number(t??d):r??f;return(0,n.createElement)("svg",{ref:h,...i,width:t??d??i.width,height:t??d??i.height,stroke:e??k,strokeWidth:C,className:(0,o.z)("lucide",w,u),...!a&&!hasA11yProp(c)&&{"aria-hidden":"true"},...c},[...l.map(([e,t])=>(0,n.createElement)(e,t)),...Array.isArray(a)?a:[a]])})},2963:function(e,t,r){"use strict";r.d(t,{z:function(){return mergeClasses}});/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let mergeClasses=(...e)=>e.filter((e,t,r)=>!!e&&""!==e.trim()&&r.indexOf(e)===t).join(" ").trim()}},function(e){e.O(0,[971,472,744],function(){return e(e.s=1242)}),_N_E=e.O()}]);
